ac4e609d6d6aee521d49a5d0e334cb5c507de85d
JayBeckly <100592633+JayBeckly@users.noreply.github.com>
2022-03-01 11:12:51 +0100

    Add files via upload

---
+++ co2 and scattered plot.R
@@ -0,0 +1,15 @@
+&&&&&co2
+co2
+head(co2)
+tail(co2)
+hist(co2,add=TRUE,col='red',main="Jaybeckly histogram")
+
+%%%%scatterPlot
+attach(co2)
+Jan
+
+plot(Jan,pch=16,cex=1,main="plot for Jan,June,July and Dec")
+
+data(co2)
+co2
+View(co2)
